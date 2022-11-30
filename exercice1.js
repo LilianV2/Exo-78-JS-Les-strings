e53cdb3a4c42904836a40e6ffdeaa8fd9b18f19a
lilia <lilianom59@outlook.fr>
2022-11-30 14:42:53 +0100

    exo string

--- exercice1.js
+++ exercice1.js
@@ -2,12 +2,20 @@ let monTexte = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do 
 
 // Completer le code suivant pour afficher la longueur de la variable monTexte
 
-alert(monTexte.);
+alert(monTexte.length);
 
 
 //Ajouter du code ci dessous pour réaliser la suite de l'exercice
 
+let texte2 = monTexte.indexOf('i');
+document.getElementById("position").innerHTML = texte2;
 
+let nouvelleChaine = monTexte.toLowerCase();
+document.getElementById('chaineMaj').innerHTML = nouvelleChaine;
+
+for (let i = 0; i < monTexte.indexOf('i'); i++) {
+    alert("la lettre a à été trouvée");
+}
 
 
 
